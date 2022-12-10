52e1a86b8e851863bc26777a8706cef8873409a5
John Grudzinski <johngrudzinski@yahoo.com>
2022-12-09 22:39:11 -0500

    Submit Message v1

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const ratingPage = document.querySelector('.rating_content_container');
 const thanksPage = document.querySelector('.thanks_content_container');
 const submitBtn = document.querySelector('#submit_btn');
 const buttons = document.querySelectorAll('li');
-
+var error = document.querySelector('.error_container');
 
 
 for (li in buttons) {
@@ -12,6 +12,7 @@ for (li in buttons) {
          btn.classList.remove('color_change');
        })
        this.classList.add('color_change');
+       error.classList.add('hidden');
    }
 }
 
@@ -21,20 +22,34 @@ submitBtn.addEventListener('click', function(){
     var items = ul.getElementsByTagName('li');
     for (var i = 0; i < items.length; i++) {
         if (items[i].classList.contains('color_change')){
-            if(thanksPage.classList.contains('hidden')) 
-            error.classList.add('hidden');
-            thanksPage.classList.remove('hidden');
-            ratingPage.classList.add('hidden');
-            
+            if(thanksPage.classList.contains('hidden')){
+                thanksPage.classList.remove('hidden');
+                ratingPage.classList.add('hidden');
+            }  
+
         }
+        else if(ratingPage.classList.contains('hidden')) {
+            error.classList.add('hidden');
+        }
+
+
         else {
             error.classList.remove('hidden');
-        }
-    
-    
+        }   
     }
-
 });
+
+var message = document.getElementById('submitted_rating');
+var ul = document.getElementById('rating_list');
+var items = ul.getElementsByTagName('li');
+for (var i = 0; i < items.length; i++) {
+    if (items[i].classList.contains('rating_1')) {
+        message.innerHTML = "You selected 1 out of 5";
+    }
+}
+
+
+
 
 /* var ul = document.getElementById('rating_list');
 var items = ul.getElementsByTagName('li');
@@ -49,5 +64,29 @@ for (var i = 0; i < items.length; i++) {
     if(thanksPage.classList.contains('hidden')) {
         thanksPage.classList.remove('hidden');
         ratingPage.classList.add('hidden');
+    }
+}); */
+
+
+
+
+
+
+/* submitBtn.addEventListener('click', function(){
+    var error = document.querySelector('.error_container');
+    var ul = document.getElementById('rating_list');
+    var items = ul.getElementsByTagName('li');
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].classList.contains('color_change')){
+            if(thanksPage.classList.contains('hidden')){
+                thanksPage.classList.remove('hidden');
+                ratingPage.classList.add('hidden');
+            }  
+
+        }
+
+        else {
+            error.classList.remove('hidden');
+        }   
     }
 }); */
